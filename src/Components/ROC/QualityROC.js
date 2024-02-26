099317cb7421b39bee26a1ee90a22d3280cd9510
Athanasia19 <christinejoy.esparas@tup.edu.ph>
2024-02-27 02:26:52 +0800

    quality charts

--- src/Components/ROC/QualityROC.js
+++ src/Components/ROC/QualityROC.js
@@ -1,59 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Chart from 'react-apexcharts';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Chart from "react-apexcharts";
 
 const QualityROC = () => {
-    const [roc, setROC] = useState({ fpr: [], tpr: [], roc_auc: 0 });
+  const [roc, setROC] = useState({ fpr: [], tpr: [], roc_auc: 0 });
 
-    useEffect(() => {
-        axios.get(`${process.env.REACT_APP_API}/quality/roc-curve`)
-            .then(response => {
-                setROC(response.data); // Change this line
-            });
-    }, []);
-    
-    const options = {
-        chart: {
-            type: 'line',
-            animations: {
-                enabled: true,
-                easing: 'easeinout',
-                speed: 20000,
-                animateGradually: {
-                    enabled: true,
-                    delay: 1000
-                },
-                dynamicAnimation: {
-                    enabled: true,
-                    speed: 10000 
-                }
-            }
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API}/quality/roc-curve`)
+      .then((response) => {
+        setROC(response.data); // Change this line
+      });
+  }, []);
+
+  const options = {
+    chart: {
+      type: "line",
+      animations: {
+        enabled: true,
+        easing: "easeinout",
+        speed: 20000,
+        animateGradually: {
+          enabled: true,
+          delay: 1000,
         },
-        title: {
-            text: `ROC Curve of Your Model (AUC = ${roc.roc_auc.toFixed(2)})`
+        dynamicAnimation: {
+          enabled: true,
+          speed: 10000,
         },
-        xaxis: {
-            categories: roc.fpr,
-            title: {
-                text: 'False Positive Rate'
-            }
-        },
-        yaxis: {
-            title: {
-                text: 'True Positive Rate'
-            }
-        },
-        legend: {
-            show: false
-        }
-    };
-    
-    const series = [{
-        name: 'ROC Curve',
-        data: roc.tpr
-    }];
-    
-    return <Chart options={options} series={series} type="line" />;
+      },
+    },
+    title: {
+      text: `ROC Curve of Your Model (AUC = ${roc.roc_auc.toFixed(2)})`,
+    },
+    xaxis: {
+      categories: roc.fpr,
+      title: {
+        text: "False Positive Rate",
+      },
+    },
+    yaxis: {
+      title: {
+        text: "True Positive Rate",
+      },
+    },
+    legend: {
+      show: false,
+    },
+  };
+
+  const series = [
+    {
+      name: "ROC Curve",
+      data: roc.tpr,
+    },
+  ];
+
+  return (
+    <Chart
+      options={options}
+      series={series}
+      type="line"
+      style={{ width: "50%", height: "50%" }}
+    />
+  );
 };
 
 export default QualityROC;
