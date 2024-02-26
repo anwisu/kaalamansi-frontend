857ef439961670834b8dbc3e5968f0ade69bef5d
Athanasia19 <christinejoy.esparas@tup.edu.ph>
2024-02-27 02:26:42 +0800

    disease charts

--- src/Components/ConfusionMatrix/DiseaseCM.js
+++ src/Components/ConfusionMatrix/DiseaseCM.js
@@ -1,68 +1,74 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import ReactApexChart from 'react-apexcharts';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ReactApexChart from "react-apexcharts";
 
 const DiseaseCM = () => {
-    const [confMatrix, setConfMatrix] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+  const [confMatrix, setConfMatrix] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const fetchData = async () => {
-        try {
-            
-            const response = await axios.get(`${process.env.REACT_APP_API}/disease/confusion-matrix`);
-            setConfMatrix(Object.values(response.data)); // Convert the dictionary to an array of arrays
-            console.log("Confusion Matrix after setting state:", confMatrix); // Convert the dictionary to an array of arrays
-            setIsLoading(false);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-            setIsLoading(false);
-        }
-    };
-
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    if (isLoading) {
-        return <div>Loading...</div>;
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API}/disease/confusion-matrix`
+      );
+      setConfMatrix(Object.values(response.data)); // Convert the dictionary to an array of arrays
+      console.log("Confusion Matrix after setting state:", confMatrix); // Convert the dictionary to an array of arrays
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setIsLoading(false);
     }
+  };
 
-    const series = confMatrix.map((row, i) => ({
-        name: `Actual:${i}`,
-        data: row.map((value, j) => ({ x: `Predicted:${j}`, y: value }))
-    }));
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const options = {
-        chart: {
-            type: 'heatmap',
-        },
-        dataLabels: {
-            enabled: true,
-            style: {
-                fontSize: '20px', // Change the font size
-                colors: ['#033200'] // Change the font color
-            },
-        },
-        theme: {
-            monochrome: {
-                enabled: true,
-                color: '#67FF53',
-                shadeIntensity: 0.1
-            },
-        },
-        title: {
-            text: 'Kalamansi Disease Confusion Matrix Heatmap'
-        },
-        xaxis: {
-            type: 'category',
-        },
-    };
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-    return (
-        <div>
-            <ReactApexChart options={options} series={series} type="heatmap" />
-        </div>
-    );
+  const series = confMatrix.map((row, i) => ({
+    name: `Actual:${i}`,
+    data: row.map((value, j) => ({ x: `Predicted:${j}`, y: value })),
+  }));
+
+  const options = {
+    chart: {
+      type: "heatmap",
+    },
+    dataLabels: {
+      enabled: true,
+      style: {
+        fontSize: "20px", // Change the font size
+        colors: ["#033200"], // Change the font color
+      },
+    },
+    theme: {
+      monochrome: {
+        enabled: true,
+        color: "#67FF53",
+        shadeIntensity: 0.1,
+      },
+    },
+    title: {
+      text: "Kalamansi Disease Confusion Matrix Heatmap",
+    },
+    xaxis: {
+      type: "category",
+    },
+  };
+
+  return (
+    <div>
+      <ReactApexChart
+        options={options}
+        series={series}
+        type="heatmap"
+        style={{ width: "100%" }}
+      />
+    </div>
+  );
 };
 
 export default DiseaseCM;
