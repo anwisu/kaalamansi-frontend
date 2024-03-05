653de36d65280cf5f0cd331438fe81dbe3fc4a63
Athanasia19 <christinejoy.esparas@tup.edu.ph>
2024-03-05 14:15:58 +0800

    home update

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,146 +1,163 @@
 import React, { Fragment, useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "../App.css";
 
 const Home = () => {
-    return (
-        <Fragment>
-            <div className="container">
-                <div className="row">
-                    <div className="hero flex justify-between items-center w-screen p-3">
-                        <div className="column" style={{ paddingLeft: "1cm" }}>
-                            <img
-                                src="./images/hero.png"
-                                className="max-w-sm rounded-lg"
-                            />
-                        </div>
-                        <div className="column" style={{ paddingLeft: "1cm" }}>
-                            <h1
-                                className="text-8xl font-bold"
-                                style={{
-                                    paddingRight: "1em",
-                                    color: "#58B741",
-                                    textAlign: "right",
-                                    fontFamily: "League Spartan",
-                                    textShadow: "2px 2px 2px rgba(0, 0, 0, 0.5)",
-                                }}
-                            >
-                                Kaalaman.si
-                            </h1>
-                            <p
-                                className="text-left"
-                                style={{ color: "#58B741", paddingRight: "5em" }}
-                            >
-                                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                                repudiandae et a id nisi. Provident cupiditate voluptatem et in.
-                                Quaerat fugiat ut assumenda excepturi exercitationem quasi. In
-                                deleniti eaque aut repudiandae et a id nisi. Provident
-                                cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-                                excepturi exercitationem quasi. In deleniti eaque aut
-                                repudiandae et a id nisi.
-                            </p>
-                            <button className="btn btn-primary btn-green">Learn More</button>
-                        </div>
-                    </div>
-                    <div className="hero w-screen px-20 pb-6">
-                        <div className="hero-content flex-col lg:flex-row-reverse items-left">
-                            <img src="./images/predict2.png" className="max-w-xs rounded-lg" />
-                            <div className="text-left pt-6">
-                                <h1
-                                    className="text-5xl font-bold pr-12"
-                                    style={{
-                                        color: "#58B741",
-                                        fontFamily: "League Spartan",
-                                        textShadow: "2px 2px 2px rgba(0, 0, 0, 0.5)",
-                                    }}
-                                >
-                                    Predict its quality and diseases
-                                </h1>
-                                <p
-                                    className="pt-6 pr-20 "
-                                    style={{
-                                        color: "#58B741",
-                                    }}
-                                >
-                                    Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                                    assumenda excepturi exercitationem quasi. In deleniti eaque
-                                    aut repudiandae et a id nisi. Provident cupiditate voluptatem
-                                    et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                                    quasi. In deleniti eaque aut repudiandae et a id nisi.
-                                    Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                                    assumenda excepturi exercitationem quasi. In deleniti eaque
-                                    aut repudiandae et a id nisi.
-                                </p>
-                                <button className="btn btn-primary btn-green">Try Now</button>
-                            </div>
-                        </div>
-                    </div>
-                    <h1
-                        className="text-3xl font-bold text-center mt-5 mb-5 w-screen p-3"
-                        style={{
-                            color: "#58B741",
-                            fontFamily: "League Spartan",
-                            textShadow: "1px 1px 2px rgba(0, 0, 0, 0.5)",
-                        }}
-                    >
-                        Kalamansi Gallery
-                    </h1>
-                    <div className="hero flex justify-between items-center w-screen pb-20">
-                        <div class="container mx-auto px-5 lg:px-32">
-                            <div class="-m-1 flex flex-wrap md:-m-2">
-                                <div class="flex w-1/2 flex-wrap">
-                                    <div class="w-1/2 p-1 md:p-2">
-                                        <img
-                                            alt="gallery"
-                                            class="block h-full w-full rounded-lg object-cover object-center"
-                                            src="./images/plant 1.jpg"
-                                        />
-                                    </div>
-                                    <div class="w-1/2 p-1 md:p-2">
-                                        <img
-                                            alt="gallery"
-                                            class="block h-full w-full rounded-lg object-cover object-center"
-                                            src="./images/plant 2.jpg"
-                                        />
-                                    </div>
-                                    <div class="w-full p-1 md:p-2">
-                                        <img
-                                            alt="gallery"
-                                            class="block h-full w-full rounded-lg object-cover object-center"
-                                            src="./images/plant 3.jpg"
-                                        />
-                                    </div>
-                                </div>
-                                <div class="flex w-1/2 flex-wrap">
-                                    <div class="w-full p-1 md:p-2">
-                                        <img
-                                            alt="gallery"
-                                            class="block h-full w-full rounded-lg object-cover object-center"
-                                            src="./images/plant 4.jpg"
-                                        />
-                                    </div>
-                                    <div class="w-1/2 p-1 md:p-2">
-                                        <img
-                                            alt="gallery"
-                                            class="block h-full w-full rounded-lg object-cover object-center"
-                                            src="./images/plant 5.jpg"
-                                        />
-                                    </div>
-                                    <div class="w-1/2 p-1 md:p-2">
-                                        <img
-                                            alt="gallery"
-                                            class="block h-full w-full rounded-lg object-cover object-center"
-                                            src="./images/plant 6.jpg"
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <Fragment>
+      <div className="container">
+        <div className="row">
+          <div className="hero flex justify-between items-center w-screen p-3">
+            <div className="column" style={{ paddingLeft: "1cm" }}>
+              <img src="./images/hero.png" className="max-w-sm rounded-lg" />
             </div>
-        </Fragment>
-    );
+            <div className="column" style={{ paddingLeft: "1cm" }}>
+              <h1
+                className="text-8xl font-bold"
+                style={{
+                  paddingRight: "1em",
+                  color: "#58B741",
+                  textAlign: "right",
+                  fontFamily: "League Spartan",
+                  textShadow: "2px 2px 2px rgba(0, 0, 0, 0.5)",
+                }}
+              >
+                Kaalaman.si
+              </h1>
+              <p
+                className="text-left"
+                style={{ color: "#58B741", paddingRight: "5em" }}
+              >
+                Kaalaman.si is a web-based system for home gardeners who grow
+                kalamansi. It uses predictive analysis, quality assessment,
+                disease assessment, and user-friendly interfaces to help users
+                optimize their urban farming. It is accessible from any device
+                and aims to make kalamansi cultivation easy and enjoyable. The
+                system uses a logistic regression model to predict the quality
+                and disease status of kalamansi fruits based on various factors
+                such as size, color, shape, and blemish.
+              </p>
+              {/* <button className="btn btn-primary btn-green"></button> */}
+              <Link
+                to="/about"
+                style={{ textDecoration: "none" }}
+                className="btn btn-green flex items-center"
+              >
+                Learn More
+              </Link>
+            </div>
+          </div>
+          <div className="hero w-screen px-20 pb-6">
+            <div className="hero-content flex-col lg:flex-row-reverse items-left">
+              <img
+                src="./images/predict2.png"
+                className="max-w-xs rounded-lg"
+              />
+              <div className="text-left pt-6">
+                <h1
+                  className="text-5xl font-bold pr-12"
+                  style={{
+                    color: "#58B741",
+                    fontFamily: "League Spartan",
+                    textShadow: "2px 2px 2px rgba(0, 0, 0, 0.5)",
+                  }}
+                >
+                  Predict its quality and diseases
+                </h1>
+                <p
+                  className="pt-6 pr-20 "
+                  style={{
+                    color: "#58B741",
+                  }}
+                >
+                  Empower your urban farming journey with Kaalaman.si, your
+                  trusted companion in home gardening. Our innovative platform
+                  utilizes cutting-edge predictive analysis to assess the
+                  quality and detect potential diseases in your kalamansi
+                  plants. Unleash the full potential of your garden as
+                  Kaalaman.si provides valuable insights and recommendations,
+                  making it easier than ever for home gardeners to achieve
+                  optimal growth and ensure the health of their precious
+                  kalamansi trees. Cultivate with confidence, backed by science
+                  and technology.
+                </p>
+                {/* <button className="btn btn-primary btn-green">Predict Now</button> */}
+                <Link
+                to="/predict/quality"
+                style={{ textDecoration: "none" }}
+                className="btn btn-green flex items-center"
+              >
+                Predict Now
+              </Link>
+              </div>
+            </div>
+          </div>
+          <h1
+            className="text-3xl font-bold text-center mt-5 mb-5 w-screen p-3"
+            style={{
+              color: "#58B741",
+              fontFamily: "League Spartan",
+              textShadow: "1px 1px 2px rgba(0, 0, 0, 0.5)",
+            }}
+          >
+            Kalamansi Gallery
+          </h1>
+          <div className="hero flex justify-between items-center w-screen pb-20">
+            <div class="container mx-auto px-5 lg:px-32">
+              <div class="-m-1 flex flex-wrap md:-m-2">
+                <div class="flex w-1/2 flex-wrap">
+                  <div class="w-1/2 p-1 md:p-2">
+                    <img
+                      alt="gallery"
+                      class="block h-full w-full rounded-lg object-cover object-center"
+                      src="./images/plant 1.jpg"
+                    />
+                  </div>
+                  <div class="w-1/2 p-1 md:p-2">
+                    <img
+                      alt="gallery"
+                      class="block h-full w-full rounded-lg object-cover object-center"
+                      src="./images/plant 2.jpg"
+                    />
+                  </div>
+                  <div class="w-full p-1 md:p-2">
+                    <img
+                      alt="gallery"
+                      class="block h-full w-full rounded-lg object-cover object-center"
+                      src="./images/plant 3.jpg"
+                    />
+                  </div>
+                </div>
+                <div class="flex w-1/2 flex-wrap">
+                  <div class="w-full p-1 md:p-2">
+                    <img
+                      alt="gallery"
+                      class="block h-full w-full rounded-lg object-cover object-center"
+                      src="./images/plant 4.jpg"
+                    />
+                  </div>
+                  <div class="w-1/2 p-1 md:p-2">
+                    <img
+                      alt="gallery"
+                      class="block h-full w-full rounded-lg object-cover object-center"
+                      src="./images/plant 5.jpg"
+                    />
+                  </div>
+                  <div class="w-1/2 p-1 md:p-2">
+                    <img
+                      alt="gallery"
+                      class="block h-full w-full rounded-lg object-cover object-center"
+                      src="./images/plant 6.jpg"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Home;
