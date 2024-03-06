e3c4a811ffdfaf63c44e04e1dbc70caad70836e5
Athanasia19 <christinejoy.esparas@tup.edu.ph>
2024-03-07 00:44:25 +0800

    update quality result

--- src/Components/Predict/QualityResult.js
+++ src/Components/Predict/QualityResult.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const QualityResult = () => {
   const [combinedQualityData, setCombinedQualityData] = useState(null);
@@ -10,7 +10,7 @@ const QualityResult = () => {
     const fetchData = async () => {
       try {
         const result = await axios(
-          `${process.env.REACT_APP_API}/predict/quality/${id}`,
+          `${process.env.REACT_APP_API}/predict/quality/${id}`
         );
         // console.log(result.data); // Log the fetched data
 
@@ -18,16 +18,15 @@ const QualityResult = () => {
         console.log(combinedQualityData);
         // Fetch additional data from the quality collection
         const qualityResult = await axios(
-          `${process.env.REACT_APP_API}/admin/quality/${result.data.reco_data.quality_id}`,
+          `${process.env.REACT_APP_API}/admin/quality/${result.data.reco_data.quality_id}`
         );
         // console.log(qualityResult.data); // Log the fetched quality data
 
-        setCombinedQualityData(prevData => ({
+        setCombinedQualityData((prevData) => ({
           ...prevData,
           qualityData: qualityResult.data,
         }));
         console.log(combinedQualityData);
-
       } catch (error) {
         console.error(error);
       }
@@ -40,10 +39,9 @@ const QualityResult = () => {
     return <div>Loading...</div>;
   }
 
-
   return (
     <div>
-      <div class="mt-8 mx-30 w-80 items-center text-center justify-between mx-auto">
+      <div class="mt-8 mx-30 items-center text-center justify-between mx-auto w-1/2">
         <h1
           class="text-3xl font-semibold mb-3 text-gray-700"
           style={{
@@ -53,11 +51,13 @@ const QualityResult = () => {
         >
           Quality Result
         </h1>
-        <div class="card1 p-10 py-10 bg-gray-200  text-center">
-          {combinedQualityData && combinedQualityData.qualityData.quality_data.predicted_quality === "low" ? (
-            <div>
-              <div className="flex items-center">
-                <span>
+        <div class="p-10 py-10 bg-gray-200 text-center w-90 rounded-2xl">
+          <div>
+            <div className="flex items-center text-center justify-center">
+              {combinedQualityData &&
+              combinedQualityData.qualityData.quality_data.predicted_quality ===
+                "low" ? (
+                <div className="flex items-center text-center justify-center">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -72,73 +72,24 @@ const QualityResult = () => {
                       d="M15.182 16.318A4.486 4.486 0 0 0 12.016 15a4.486 4.486 0 0 0-3.198 1.318M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z"
                     />
                   </svg>
-                </span>
-                <p
-                  class="text-red-500 text-3xl mb-2  pl-3 font-bold text-center"
-                  style={{
-                    fontFamily: "League Spartan",
-                    textShadow: "1px 1px 1px rgba(0, 0, 0, 0.5)",
-                  }}
-                >
-                  Low Quality
-                </p>
-              </div>
-
-              <p class="text-gray-600 text-sm">
-                <b>Quality ID:</b> {combinedQualityData.reco_data.quality_id}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Size:</b> {combinedQualityData.qualityData.quality_data.size}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Shape:</b> {combinedQualityData.qualityData.quality_data.shape}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Firmness:</b> {combinedQualityData.qualityData.quality_data.firmness}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Skin Color:</b> {combinedQualityData.qualityData.quality_data.skin_color}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Blemishes:</b> {combinedQualityData.qualityData.quality_data.blemishes}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Soil Type:</b> {combinedQualityData.qualityData.quality_data.soil_type}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Sun Exposure:</b> {combinedQualityData.qualityData.quality_data.sun_exposure}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Location:</b> {combinedQualityData.qualityData.quality_data.location}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Fertilized:</b> {combinedQualityData.qualityData.quality_data.fertilized}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Watering Schedule:</b> {combinedQualityData.qualityData.quality_data.watering_sched}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Pruning:</b> {combinedQualityData.qualityData.quality_data.pruning}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Pest Presence:</b> {combinedQualityData.qualityData.quality_data.pest_presence}
-              </p>
-              <p class="text-gray-600 text-sm">
-                {combinedQualityData.reco_data.soil_recommendation && (
-                  <><b>Soil Recommendations:</b> {combinedQualityData.reco_data.soil_recommendation}<br /></>
-                )}
-                {combinedQualityData.reco_data.sun_recommendation && (
-                  <><b>Sun Recommendations:</b> {combinedQualityData.reco_data.sun_recommendation}<br /></>
-                )}
-                {combinedQualityData.reco_data.watering_recommendation && (
-                  <><b>Watering Recommendations:</b> {combinedQualityData.reco_data.watering_recommendation}<br /></>
-                )}
-              </p>
-            </div>
-          ) : (
-            <div>
-              <div className="flex items-center">
-                <span>
+                  <span>
+                    <p
+                      class="text-red-500 text-3xl mb-2  pl-3 font-bold text-center capitalize"
+                      style={{
+                        fontFamily: "League Spartan",
+                        textShadow: "1px 1px 1px rgba(0, 0, 0, 0.5)",
+                      }}
+                    >
+                      {
+                        combinedQualityData.qualityData.quality_data
+                          .predicted_quality
+                      }{" "}
+                      Quality
+                    </p>
+                  </span>
+                </div>
+              ) : (
+                <div className="flex items-center">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -153,74 +104,115 @@ const QualityResult = () => {
                       d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z"
                     />
                   </svg>
-                </span>
-                <p
-                  class="text-green-500 text-3xl mb-2  pl-3 font-bold text-center"
-                  style={{
-                    color: "#4AA032",
-                    fontFamily: "League Spartan",
-                    textShadow: "1px 1px 1px rgba(0, 0, 0, 0.5)",
-                  }}
-                >
-                  High Quality
+                  <span>
+                    <p
+                      class="text-green-500 text-3xl mb-2  pl-3 font-bold text-center capitalize"
+                      style={{
+                        fontFamily: "League Spartan",
+                        textShadow: "1px 1px 1px rgba(0, 0, 0, 0.5)",
+                      }}
+                    >
+                      {
+                        combinedQualityData.qualityData.quality_data
+                          .predicted_quality
+                      }{" "}
+                      Quality
+                    </p>
+                  </span>
+                </div>
+              )}
+            </div>
+
+            <p class="text-gray-600 text-sm">
+              <b>Quality ID:</b> {combinedQualityData.reco_data.quality_id}
+            </p>
+            <hr class="my-4 border-t-2 border-gray-400" />
+
+            <div className="grid grid-cols-3 gap-4 text-left">
+              <div className="col-span-1">
+                <p className="text-gray-600 text-sm">
+                  <b>Size:</b>{" "}
+                  {combinedQualityData.qualityData.quality_data.size}
+                </p>
+                <p className="text-gray-600 text-sm">
+                  <b>Shape:</b>{" "}
+                  {combinedQualityData.qualityData.quality_data.shape}
+                </p>
+                <p className="text-gray-600 text-sm">
+                  <b>Firmness:</b>{" "}
+                  {combinedQualityData.qualityData.quality_data.firmness}
+                </p>
+                <p className="text-gray-600 text-sm">
+                  <b>Skin Color:</b>{" "}
+                  {combinedQualityData.qualityData.quality_data.skin_color}
+                </p>
+                <p className="text-gray-600 text-sm">
+                  <b>Blemishes:</b>{" "}
+                  {combinedQualityData.qualityData.quality_data.blemishes}
                 </p>
               </div>
 
-              <p class="text-gray-600 text-sm">
-                <b>Quality ID:</b> {combinedQualityData.reco_data.quality_id}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Size:</b> {combinedQualityData.qualityData.quality_data.size}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Shape:</b> {combinedQualityData.qualityData.quality_data.shape}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Firmness:</b> {combinedQualityData.qualityData.quality_data.firmness}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Skin Color:</b> {combinedQualityData.qualityData.quality_data.skin_color}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Blemishes:</b> {combinedQualityData.qualityData.quality_data.blemishes}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Soil Type:</b> {combinedQualityData.qualityData.quality_data.soil_type}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Sun Exposure:</b> {combinedQualityData.qualityData.quality_data.sun_exposure}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Location:</b> {combinedQualityData.qualityData.quality_data.location}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Fertilized:</b> {combinedQualityData.qualityData.quality_data.fertilized}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Watering Schedule:</b> {combinedQualityData.qualityData.quality_data.watering_sched}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Pruning:</b> {combinedQualityData.qualityData.quality_data.pruning}
-              </p>
-              <p class="text-gray-600 text-sm">
-                <b>Pest Presence:</b> {combinedQualityData.qualityData.quality_data.pest_presence}
-              </p>
-              <p class="text-gray-600 text-sm">
-                {combinedQualityData.reco_data.soil_recommendation && (
-                  <><b>Soil Recommendations:</b> {combinedQualityData.reco_data.soil_recommendation}<br /></>
-                )}
-                {combinedQualityData.reco_data.sun_recommendation && (
-                  <><b>Sun Recommendations:</b> {combinedQualityData.reco_data.sun_recommendation}<br /></>
-                )}
-                {combinedQualityData.reco_data.watering_recommendation && (
-                  <><b>Watering Recommendations:</b> {combinedQualityData.reco_data.watering_recommendation}<br /></>
-                )}
-              </p>
+              <div className="col-span-1">
+                <p className="text-gray-600 text-sm">
+                  <b>Soil Type:</b>{" "}
+                  {combinedQualityData.qualityData.quality_data.soil_type}
+                </p>
+                <p className="text-gray-600 text-sm">
+                  <b>Sun Exposure:</b>{" "}
+                  {combinedQualityData.qualityData.quality_data.sun_exposure}
+                </p>
+                <p className="text-gray-600 text-sm">
+                  <b>Location:</b>{" "}
+                  {combinedQualityData.qualityData.quality_data.location}
+                </p>
+                <p className="text-gray-600 text-sm">
+                  <b>Fertilized:</b>{" "}
+                  {combinedQualityData.qualityData.quality_data.fertilized}
+                </p>
+                <p className="text-gray-600 text-sm">
+                  <b>Watering Schedule:</b>{" "}
+                  {combinedQualityData.qualityData.quality_data.watering_sched}
+                </p>
+              </div>
+
+              <div className="col-span-1">
+                <p className="text-gray-600 text-sm">
+                  <b>Pruning:</b>{" "}
+                  {combinedQualityData.qualityData.quality_data.pruning}
+                </p>
+                <p className="text-gray-600 text-sm">
+                  <b>Pest Presence:</b>{" "}
+                  {combinedQualityData.qualityData.quality_data.pest_presence}
+                </p>
+                <p className="text-gray-600 text-sm">
+                  {combinedQualityData.reco_data.soil_recommendation && (
+                    <>
+                      <b>Soil Recommendations:</b>{" "}
+                      {combinedQualityData.reco_data.soil_recommendation}
+                      <br />
+                    </>
+                  )}
+                  {combinedQualityData.reco_data.sun_recommendation && (
+                    <>
+                      <b>Sun Recommendations:</b>{" "}
+                      {combinedQualityData.reco_data.sun_recommendation}
+                      <br />
+                    </>
+                  )}
+                  {combinedQualityData.reco_data.watering_recommendation && (
+                    <>
+                      <b>Watering Recommendations:</b>{" "}
+                      {combinedQualityData.reco_data.watering_recommendation}
+                      <br />
+                    </>
+                  )}
+                </p>
+              </div>
             </div>
-          )}
+          </div>
         </div>
       </div>
-      <div class="mt-10 items-center mx-auto pl-32 ml-80">
+      <div class="mt-10 items-center mx-auto text-center justify-center ">
         <div class=" btn btn-success mr-10 rounded-full btns">
           <span class="icons">
             <svg
