ef6b415ed79ab5fe45cf96408d646effb3bcf9b9
anwisu <cabauatan.jianellaguia@gmail.com>
2024-03-08 23:47:32 +0800

    Update UserQualityPredictions.js

--- src/Components/User/UserQualityPredictions.js
+++ src/Components/User/UserQualityPredictions.js
@@ -84,87 +84,93 @@ const UserQualityPredictions = () => {
                             />
                         )}
                     </ThemeProvider> */}
-                    {itemsToShow.map((prediction, index) => (
-                        <Accordion key={index} open={open === index + 1} className="mb-2 rounded-lg border border-blue-gray-100 px-4">
-                            <AccordionHeader
-                                onClick={() => handleOpen(index + 1)}
-                                className={`border-b-0 transition-colors ${open === index + 1 ? "text-green-500 hover:!text-green-700" : ""}`}
-                            >
-                                <div className="flex justify-between w-full">
-                                    <div>
-                                        {prediction.quality_data.predicted_quality.charAt(0).toUpperCase() + prediction.quality_data.predicted_quality.slice(1).toLowerCase()}{" "} Quality
-                                    </div>
-                                    <div>
-                                        Predicted On: {String(prediction.created_at).substring(0, 16)}
-                                    </div>
-                                </div>
-                                {/* {index + 1} */}
-                            </AccordionHeader>
-                            <AccordionBody className="pt-0 text-base font-normal">
-                                <div className="flex">
-                                    <div className="flex-1">
-                                        <h2>Fruit Appearance</h2>
-                                        Size: {prediction.quality_data.size} <br />
-                                        Firmness: {prediction.quality_data.firmness} <br />
-                                        Shape: {prediction.quality_data.shape}<br />
-                                        Skin Color: {prediction.quality_data.skin_color}<br />
-                                        Blemishes: {prediction.quality_data.blemishes}<br />
-                                    </div>
-                                    <div className="flex-1">
-                                        <h2>Environmental Factors</h2>
-                                        Soil Type: {prediction.quality_data.soil_type} <br />
-                                        Sun Exposure: {prediction.quality_data.sun_exposure} <br />
-                                        Location: {prediction.quality_data.location} <br />
-                                        Pest Presence: {prediction.quality_data.pest_presence} <br />
-                                    </div>
-                                    <div className="flex-1">
-                                        <h2>Cultivation Practices</h2>
-                                        Fertilized: {prediction.quality_data.fertilized} <br />
-                                        Watering Schedule: {prediction.quality_data.watering_sched} <br />
-                                        Pruning: {prediction.quality_data.pruning} <br />
-                                    </div>
-                                </div>
-                                <br />
-                                <hr />
-                                <br />
-                                <h2>Recommendations</h2>
-                                {prediction.soil_recommendation && (
-                                    <div>Soil Recommendation: {prediction.soil_recommendation}</div>
-                                )}
-                                {prediction.watering_recommendation && (
-                                    <div>Watering Recommendation: {prediction.watering_recommendation}</div>
-                                )}
-                                {prediction.sun_recommendation && (
-                                    <div>Sun Recommendation: {prediction.sun_recommendation}</div>
-                                )}
+                    {itemsToShow.length > 0 ? (
+                        itemsToShow.map((prediction, index) => (
+                            <Fragment>
+                                <Accordion key={index} open={open === index + 1} className="mb-2 rounded-lg border border-blue-gray-100 px-4">
+                                    <AccordionHeader
+                                        onClick={() => handleOpen(index + 1)}
+                                        className={`border-b-0 transition-colors ${open === index + 1 ? "text-green-500 hover:!text-green-700" : ""}`}
+                                    >
+                                        <div className="flex justify-between w-full">
+                                            <div>
+                                                {prediction.quality_data.predicted_quality.charAt(0).toUpperCase() + prediction.quality_data.predicted_quality.slice(1).toLowerCase()}{" "} Quality
+                                            </div>
+                                            <div>
+                                                Predicted On: {String(prediction.created_at).substring(0, 16)}
+                                            </div>
+                                        </div>
+                                        {/* {index + 1} */}
+                                    </AccordionHeader>
+                                    <AccordionBody className="pt-0 text-base font-normal">
+                                        <div className="flex">
+                                            <div className="flex-1">
+                                                <h2>Fruit Appearance</h2>
+                                                Size: {prediction.quality_data.size} <br />
+                                                Firmness: {prediction.quality_data.firmness} <br />
+                                                Shape: {prediction.quality_data.shape}<br />
+                                                Skin Color: {prediction.quality_data.skin_color}<br />
+                                                Blemishes: {prediction.quality_data.blemishes}<br />
+                                            </div>
+                                            <div className="flex-1">
+                                                <h2>Environmental Factors</h2>
+                                                Soil Type: {prediction.quality_data.soil_type} <br />
+                                                Sun Exposure: {prediction.quality_data.sun_exposure} <br />
+                                                Location: {prediction.quality_data.location} <br />
+                                                Pest Presence: {prediction.quality_data.pest_presence} <br />
+                                            </div>
+                                            <div className="flex-1">
+                                                <h2>Cultivation Practices</h2>
+                                                Fertilized: {prediction.quality_data.fertilized} <br />
+                                                Watering Schedule: {prediction.quality_data.watering_sched} <br />
+                                                Pruning: {prediction.quality_data.pruning} <br />
+                                            </div>
+                                        </div>
+                                        <br />
+                                        <hr />
+                                        <br />
+                                        <h2>Recommendations</h2>
+                                        {prediction.soil_recommendation && (
+                                            <div>Soil Recommendation: {prediction.soil_recommendation}</div>
+                                        )}
+                                        {prediction.watering_recommendation && (
+                                            <div>Watering Recommendation: {prediction.watering_recommendation}</div>
+                                        )}
+                                        {prediction.sun_recommendation && (
+                                            <div>Sun Recommendation: {prediction.sun_recommendation}</div>
+                                        )}
 
-                            </AccordionBody>
-                        </Accordion>
-                    ))}
-                </div>
-                <div className="flex items-center gap-4 justify-center">
-                <Button
-                        variant="text"
-                        className="flex items-center gap-2 rounded-full"
-                        onClick={prev}
-                        disabled={active === 1}
-                    >
-                        <ArrowLeftIcon strokeWidth={2} className="h-4 w-4" /> Previous
-                    </Button>
-                    <div className="flex items-center gap-2">
-                        {Array.from({ length: Math.ceil(mePredictions.length / ITEMS_PER_PAGE) }, (_, i) => (
-                            <IconButton {...getItemProps(i + 1)}>{i + 1}</IconButton>
-                        ))}
-                    </div>
-                    <Button
-                        variant="text"
-                        className="flex items-center gap-2 rounded-full"
-                        onClick={next}
-                        disabled={active === Math.ceil(mePredictions.length / ITEMS_PER_PAGE)}
-                    >
-                        Next
-                        <ArrowRightIcon strokeWidth={2} className="h-4 w-4" />
-                    </Button>
+                                    </AccordionBody>
+                                </Accordion>
+                                <div className="flex items-center gap-4 justify-center">
+                                    <Button
+                                        variant="text"
+                                        className="flex items-center gap-2 rounded-full"
+                                        onClick={prev}
+                                        disabled={active === 1}
+                                    >
+                                        <ArrowLeftIcon strokeWidth={2} className="h-4 w-4" /> Previous
+                                    </Button>
+                                    <div className="flex items-center gap-2">
+                                        {Array.from({ length: Math.ceil(mePredictions.length / ITEMS_PER_PAGE) }, (_, i) => (
+                                            <IconButton {...getItemProps(i + 1)}>{i + 1}</IconButton>
+                                        ))}
+                                    </div>
+                                    <Button
+                                        variant="text"
+                                        className="flex items-center gap-2 rounded-full"
+                                        onClick={next}
+                                        disabled={active === Math.ceil(mePredictions.length / ITEMS_PER_PAGE)}
+                                    >
+                                        Next
+                                        <ArrowRightIcon strokeWidth={2} className="h-4 w-4" />
+                                    </Button>
+                                </div>
+                            </Fragment>
+                        ))
+                    ) : (
+                        <div>No data available</div>
+                    )}
                 </div>
             </div>
         </div>
